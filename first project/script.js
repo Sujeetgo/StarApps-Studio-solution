044172f69916428d94306aa767c6e62aac304e18
sujeet kumar <sujeetkr2508@gmail.com>
2023-04-20 11:54:44 +0530

    spining of icon added

--- first project/script.js
+++ first project/script.js
@@ -4,6 +4,10 @@ const buttonBlue = document.getElementById("blue");
 const buttonPink = document.getElementById("pink");
 const buttonYellow = document.getElementById("yello");
 
+let uploadIcon = document.getElementById("ico");
+const formEl = document.querySelector("form");
+
+const uploadLogo = document.getElementById("upload-logo");
 buttonPink.addEventListener("click", ()=>{
     blueUmb.src = "Pink umbrella.png";
 })
@@ -13,4 +17,11 @@ buttonBlue.addEventListener("click", ()=>{
 })
 buttonYellow.addEventListener("click", ()=>{
     blueUmb.src = "Yello umbrella.png"; 
+})
+
+uploadLogo.addEventListener("click", (e)=>{
+    e.preventDefault();
+    uploadIcon.src = "loader_icon.svg";
+    uploadLogo.style.height ="35px";
+    uploadIcon.classList.add('rotating');
 })
